45a2a7d4fc3dcf71f7d2335fc40bd5211036183c
Meet-Jariwala34 <officialmeetjariwala@gmail.com>
2026-04-30 16:03:42 +0530

    Make a global list for adminId and VoterId

--- VotingSystem.c
+++ VotingSystem.c
@@ -1,5 +1,16 @@
 #include<stdio.h>
+
+//Global variables 
+int adminIdList[] = {1001, 1002};
+int voterIdList[] = {101, 102, 103, 104, 105, 106, 107, 108, 109, 110};
+
 void main() {
     //initializing the main file
+
+
+
     printf("Let's Start coding ...");
 }
+
+
+
