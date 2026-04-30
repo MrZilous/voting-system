d68ecf4f16ee0cd2dfebbfb32d16dbded5c5d0ab
MrZilous <nityajariwala22@gmail.com>
2026-04-30 16:04:09 +0530

    create adminchoice and checkadmin funcitons

--- VotingSystem.c
+++ VotingSystem.c
@@ -1,5 +1,54 @@
 #include<stdio.h>
-void main() {
-    //initializing the main file
-    printf("Let's Start coding ...");
+// Admin Control Panel
+// ------------------------start of function ----------------------------------
+int adminchoice()
+{
+    int choice;
+    printf("Choices : \n enter 0 for stop voting \n enter 1 for continue voting\n");
+    printf("Enter your choice : ");
+    scanf("%d", &choice);
+
+    if(choice == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+// ------------------------end of functions --------------------------------
+
+// Admin verification function 
+//-------------------------start of function---------------------------------
+int checkAdmin(int id, int list[])
+{
+
+    int n = 3;
+    //iterate to each id
+    for(int i = 0 ; i < n ; i++){
+        if(list[i] == id){
+            printf("The Admin Found !!");
+            adminchoice();
+            return 1;
+        }
+    }
+
+    printf("Admin not found !!");
+
+    return 0;
+}
+//------------------------- end of function---------------------------------
+
+
+
+void main() 
+{
+    int ids[5];
+    ids[0] = 12345;
+    ids[1] = 2345;
+    ids[2] = 990;
+
+    checkAdmin(1000, ids);
+
 }
